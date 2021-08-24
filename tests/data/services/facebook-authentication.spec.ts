0c64c36b262880ef459d057620c66e6abc013c29
Uezima <guilherme@uezima.com>
2021-08-23 21:12:57 -0300

    refactor: change teste strategy to use jest mock extended

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,11 +1,16 @@
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthenticationService } from '@/data/services/'
+import { mock } from 'jest-mock-extended'
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
 
 describe('FacebookAuthenticationService', () => {
   it('should call LoadFacebookUserApi with correct parameters', async () => {
+    /*
     const loadFacebookUserApi = {
       loadUser: jest.fn()
     }
+    */
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     await sut.exec({ token: 'any_token' })
@@ -15,11 +20,9 @@ describe('FacebookAuthenticationService', () => {
   })
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
-    const loadFacebookUserApi = {
-      loadUser: jest.fn()
-    }
+    const loadFacebookUserApi = mock<LoadFacebookUserApi>()
 
-    loadFacebookUserApi.loadUser.mockReturnValueOnce(undefined)
+    loadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
     const sut = new FacebookAuthenticationService(loadFacebookUserApi)
 
     const authResult = await sut.exec({ token: 'any_token' })
